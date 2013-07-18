eb9670b380c3fecb8e856e601925ffc24be7a299
Jon Nolen <jon.nolen@gmail.com>
2013-07-18 11:50:05 -0400

    updating template with useful bits from passageways project.

--- SinatraMockServer.xctemplate/server.rb
+++ SinatraMockServer.xctemplate/server.rb
@@ -1,21 +1,51 @@
+require 'rubygems'
+require 'active_support/core_ext'
 require 'sinatra/base'
+require 'sinatra/reloader'
+require 'sinatra/streaming'
 require 'json'
+require 'rabl'
+require 'haml'
 
 class TestServer < Sinatra::Base
 
   self.app_file = __FILE__
+
+  configure :development do
+    use Rack::Logger
+    set :logging, true
+    register Sinatra::Reloader
+  end
 
   configure do
     set :logging, true
     set :dump_errors, true
     puts "#{root}"
     set :public_folder, Proc.new { File.expand_path(File.join(root, 'fixtures')) }
+    set :haml, :format=>:html5
+    Rabl.register!
   end
 
-  def render_fixture(filename)
-    send_file File.join(settings.public_folder, filename)
+  Rabl.configure do |config|
+    config.include_json_root = false
+    config.include_child_root = false
   end
 
+  helpers Sinatra::Streaming
+  helpers do    
+    def rabl(template)
+      render :rabl, template, :format=>:json
+    end
+
+    def render_fixture(filename)
+      send_file File.join(settings.public_folder, filename)
+    end
+
+    def logger
+      return request.logger
+    end
+  end
+  
   get '/' do
     render_fixture('fixture1.json')
   end 
@@ -29,7 +59,7 @@ class TestServer < Sinatra::Base
   get '/error' do
     status 400
     content_type 'application/json'
-    {:error=>"An error occurred!!"}.json
+    {:error=>"An error occurred!!"}.to_json
   end
 
   #start the server if ruby file executed directly
